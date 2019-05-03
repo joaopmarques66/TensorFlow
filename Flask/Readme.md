7af018def187a20259c500f4668980a5520a9c75
João Marques <marques.joaopereira@gmail.com>
2019-05-03 19:08:13 +0100

    Changed requirements

---
+++ Flask/Readme.md
@@ -0,0 +1,12 @@
+# Readme
+
+# DNS
+` kookapp.northeurope.cloudapp.azure.com `
+
+
+# Server config
+1. `sudo apt install python3-venv`
+2. `cd ~/ && git clone https://github.com/joaopmarques66/TensorFlow.git`
+3. `cd ~/TensorFlow && python3 -m venv venv`
+4. `source ./venv/bin/activate`
+5. 
--- TransferLearning/tflite.py
+++ TransferLearning/tflite.py
@@ -2,5 +2,6 @@ import tensorflow as tf
 VERSION=1
 PATH = './export_model/{}'.format(VERSION)
 converter = tf.lite.TFLiteConverter.from_saved_model(PATH)
+converter.post_training_quantize = True
 tflite_model = converter.convert()
-open("converted_model.tflite", "wb").write(tflite_model)
+open("converted_model1.tflite", "wb").write(tflite_model)
